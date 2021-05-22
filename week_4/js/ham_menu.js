fe77baed0aec929efe0ffaaacebb2ba04355518c
martinnkala <nka19002@byui.edu>
2021-05-22 12:10:10 -0600

    complete

--- week_4/js/ham_menu.js
+++ week_4/js/ham_menu.js
@@ -1,6 +1,18 @@
 window.addEventListener('load', () => {
     const hambutton = document.querySelector('.ham');
-    const mainnav = document.querySelector('.topnav');
+    const mainnav = document.querySelector('#nav');
+    const datefooter = document.querySelector('.time')
 
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')},false)
+
+    const days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                    'August', 'September', 'October', 'November', 'December']
+
+    const d = new Date();
+    const day = d.getDay()
+    const month = d.getMonth()
+    const date = d.getDate()
+    const year = d.getFullYear()
+    datefooter.innerHTML = `${days[day]}, ${date} ${months[month]} ${year}`
 });
