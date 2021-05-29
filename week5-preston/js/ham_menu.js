1d4fa84d5753d9d65f5e99920209de3c5618f392
martinnkala <nka19002@byui.edu>
2021-05-29 05:36:33 -0600

    Add summary placeholder

--- week5-preston/js/ham_menu.js
+++ week5-preston/js/ham_menu.js
@@ -15,4 +15,12 @@ window.addEventListener('load', () => {
     const date = d.getDate()
     const year = d.getFullYear()
     datefooter.innerHTML = `${days[day]}, ${date} ${months[month]} ${year}`
+    
+    const pancake_day = new Date()
+    const pan_day = pancake_day.getDay()
+    const pday = document.querySelector('.pancakes')
+    if (pan_day === 5) {pday.style.display = 'block'}
+    else {pday.style.display = 'none'}
+    
+
 });
