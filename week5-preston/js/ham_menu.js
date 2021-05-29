a2f9a41d4808a9723958ced3a0bab41dcd20cd37
martinnkala <nka19002@byui.edu>
2021-05-29 14:58:45 -0600

    lil progress

--- week5-preston/js/ham_menu.js
+++ week5-preston/js/ham_menu.js
@@ -19,8 +19,6 @@ window.addEventListener('load', () => {
     const pancake_day = new Date()
     const pan_day = pancake_day.getDay()
     const pday = document.querySelector('.pancakes')
-    if (pan_day === 5) {pday.style.display = 'block'}
+    if (pan_day === 6) {pday.style.display = 'block'}
     else {pday.style.display = 'none'}
-    
-
 });
