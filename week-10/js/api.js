c8545e474b3bdd7306e231033ff1c110c0bcbab6
martinnkala <nka19002@byui.edu>
2021-06-26 12:05:18 -0600

    Week-Assignment

--- week-10/js/api.js
+++ week-10/js/api.js
@@ -1,17 +1,14 @@
-async function WeatherInfo() {
-    const requestURL = "https://api.openweathermap.org/data/2.5/weather?q=Rexburg,83440&appid=c11a593471720ea004ec8126ba37b91b&unit=imperial";
-    const response = await fetch(url);
-    if (response.status == 200) {
-        return response.json
-    } else {
-        throw new error("Error:" + response.status);
-    }
+async function weatherInfo() {
+    const requestURL = "https://api.openweathermap.org/data/2.5/weather?q=Rexburg,83440&appid=c11a593471720ea004ec8126ba37b91b&units=imperial";
+    const response = await fetch(requestURL);
+    const data = await response.json()
+    console.log(data)
+        // if (response.status == 200) {
+        //     return data
+        // } else {
+        //     throw new error("Error:" + response.status);
+        // }
 }
+weatherInfo()
 
-function retrieveWeather() {
-    const weather = WeatherInfo()
-        .then(function(Rexburgweather) {
-            console.log(retrieveWeather);
-        });
-}
-window.addEventListener('load', WeatherInfo());
+console.log('Hi')
